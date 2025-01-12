48f7ae00d1c1cce37dbd5f496fb10734e0cdaec8
Konrad <kw@paceteq.com>
2025-01-12 11:47:34 +0100

    type: add tactics

---
+++ src/types/tactics.ts
@@ -0,0 +1,28 @@
+import { Player } from "./player";
+
+export type Tactics = {
+  TW?: Player;
+  VR?: Player;
+  VZR?: Player;
+  VZ?: Player;
+  VZL?: Player;
+  VL?: Player;
+  FVR?: Player;
+  DMZR?: Player;
+  DM?: Player;
+  DMZL?: Player;
+  FVL?: Player;
+  MR?: Player;
+  MZR?: Player;
+  MZ?: Player;
+  MZL?: Player;
+  ML?: Player;
+  OMR?: Player;
+  OMZR?: Player;
+  OM?: Player;
+  OMZL?: Player;
+  OML?: Player;
+  STZR?: Player;
+  STZ?: Player;
+  STR?: Player;
+};
